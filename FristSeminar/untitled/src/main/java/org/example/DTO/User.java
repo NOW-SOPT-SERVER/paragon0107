66cdb9a7d2e92882688401902d3f1983bf98f8da
smg0725 <smg0725@naver.com>
2024-04-12 18:38:35 +0900

    로그인 및 회원가입 구현, 에러 핸들링 아직 미구현

--- FristSeminar/untitled/src/main/java/org/example/DTO/User.java
+++ FristSeminar/untitled/src/main/java/org/example/DTO/User.java
@@ -1,9 +1,20 @@
 package org.example.DTO;
 
+import java.util.Objects;
+import org.example.Repogitory.UserRepository;
+
 public record User(
         String name,
         String id,
-        int password
+        int password,
+        Bank bank,
+        Account account
 ) {
+    public User(String name,String id,int password){
+        this(name,id,password,null,null);
+    }
+    public static User create(String name,String id,int password){
+        return new User(name,id,password);
+    }
 
 }
--- FristSeminar/untitled/src/main/java/org/example/Service/BankService.java
+++ FristSeminar/untitled/src/main/java/org/example/Service/BankService.java
@@ -1,6 +1,5 @@
 package org.example.Service;
 
-import org.example.Bank.Service.BankServiceTemp;
 
 public class BankService {
     private static final BankService bankService = new BankService();
@@ -8,6 +7,10 @@ public class BankService {
         return bankService;
     }
     public void startBank(){
-
+        System.out.println("스타트뱅크");
+    }
+    public void quit(){
+        System.out.println("프로그램을 종료합니다.");
+        System.exit(0);
     }
 }
